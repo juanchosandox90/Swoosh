8cc9c5b14f1511808dc6c570d1615ff1cc306fdb
Juan Camilo Sandoval Devia <juansandoval@rccl.com>
2018-02-22 10:00:39 -0500

    Skills activity layout Intent to Skills activity

--- app/src/main/java/com/swooshkotlin/juansandoval/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/swooshkotlin/juansandoval/swoosh/LeagueActivity.kt
@@ -3,16 +3,44 @@ package com.swooshkotlin.juansandoval.swoosh
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
-    fun leagueNextClicked(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+    fun onMensClicked(view: View) {
+        womensToogleBtn.isChecked = false
+        coedToogleBtn.isChecked = false
+        selectedLeague = "Mens"
     }
+
+    fun onWomensClicked(view: View) {
+        mensToogleBtn.isChecked = false
+        coedToogleBtn.isChecked = false
+        selectedLeague = "Womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        mensToogleBtn.isChecked = false
+        womensToogleBtn.isChecked = false
+        selectedLeague = "Co-ed"
+    }
+
+    fun leagueNextClicked(view: View) {
+        if (selectedLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this, "You have to choose a League", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
 }
